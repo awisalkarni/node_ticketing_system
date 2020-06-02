2d82824210bf357ad2c4e8278e1d70c42a1bfd8f
awis-mbp <awis@awislabs.com>
2020-06-02 16:13:10 +0800

    update user model

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,12 +9,53 @@ router.route('/').get((req, res) => {
 
 router.route('/add').post((req, res) => {
   const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const phone_number = req.body.phone_number;
+  const company = req.body.company;
 
-  const newUser = new User({username});
+  const newUser = new User({ 
+    username,
+    email,
+    password,
+    phone_number,
+    company,
+   });
 
   newUser.save()
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/login').get((req, res) => {
+
+});
+
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+      .then(user => res.json(user))
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+      .then(() => res.json('User deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  User.findById(req.params.id)
+      .then(user => {
+          user.username = req.body.username;
+          user.email = req.body.email;
+          user.phone_number = req.body.phone_number;
+          user.company = req.body.company;
+
+          user.save()
+              .then(() => res.json('User updated!'))
+              .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
